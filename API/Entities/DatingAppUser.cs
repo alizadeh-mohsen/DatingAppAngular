272285fa2c64a0a7401be335829d42e715d9459f
Mohsen Alizadeh <kelarens@gmail.com>
2025-05-17 11:01:52 +0100

    Add project files.

---
+++ API/Entities/DatingAppUser.cs
@@ -0,0 +1,12 @@
+﻿namespace API.Entities
+{
+    public class DatingAppUser
+    {
+        public int MyProperty { get; set; }
+        public required string UserName { get; set; }
+        public required string KnownAs { get; set; }
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+        public DateTime LastActive { get; set; } = DateTime.UtcNow;
+            
+    }
+}
