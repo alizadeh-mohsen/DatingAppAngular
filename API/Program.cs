c535e7d33b74c9bc80ba1126847daaa7d95fe264
Mohsen Alizadeh <kelarens@gmail.com>
2025-05-17 14:33:27 +0100

    Added Cors

--- API/Program.cs
+++ API/Program.cs
@@ -8,12 +8,15 @@ builder.Services.AddDbContext<DatingContext>(optionsAction: options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+builder.Services.AddCors();
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
+
 // Configure the HTTP request pipeline.
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
+
 
 //app.UseHttpsRedirection();
 
